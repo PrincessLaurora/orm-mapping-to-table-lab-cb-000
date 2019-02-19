ad803d004d36188f77efe26083065ad43927cca3
Laura Felce <crepu83@yahoo.it>
2019-02-19 15:11:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,23 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  initialize
+
+
+
+
+ 
   
 end
+
+
+
+
+
+
+
+
+# Remember, you can access your database connection anywhere in this class
+#  with DB[:conn] 
